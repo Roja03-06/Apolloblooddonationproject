1a51d60a3e35ec06bf5264b96104605c221ae57e
Roja03-06 <85451419+Roja03-06@users.noreply.github.com>
2022-10-28 18:50:17 +0530

    Update view_receiver.c

--- project/CUT/src/view_receiver.c
+++ project/CUT/src/view_receiver.c
@@ -11,29 +11,29 @@
 #include<string.h>
 void view_receiver( )
 {
- FILE *fp;
- fp=fopen("../data/receiver.txt","r");
- if(fp==NULL)
- {
-  printf("File doesnot exits");
- }
- else
- {
-  char rname[MAX],bgroup[MAX};
-         int age;
-         long long aadharno;
-  printf("\n*************************************************\n");
-  printf(" DETAILS OF RECEIVER ");
-  printf("\n**************************************************\n");
-  r_reg *start=NULL;
-  printf("\n%20s%-20s%-20s%-20s","Aadhar no","Receiver Name","Age","Bloof Group");
-  scanf(fp,"%lld%s%d%s",&aadharno,rname,,&age,bgroup);
-  while(!feof(fp))
-  {
-   printf("\n%20lld%-20s%-20d%s ",aadharno,rname,age,bgroup);
-   fscanf(fp,"%lld%s%d%s",aadharno,rname,&age,bgroup);
-  }
-  fclose(fp);
-          }
-          menu_admin();
+         FILE *fp;
+         fp=fopen("../data/receiver.txt","r");
+         if(fp==NULL)
+         {
+                 printf("File doesnot exits");
+         }
+        else
+        {
+                   char rname[MAX],bgroup[MAX};
+                   int age;
+                   long long aadharno;
+                   printf("\n*************************************************\n");
+                   printf(" DETAILS OF RECEIVER ");
+                   printf("\n**************************************************\n");
+                   r_reg *start=NULL;
+                   printf("\n%20s%-20s%-20s%-20s","Aadhar no","Receiver Name","Age","Bloof Group");
+                   scanf(fp,"%lld%s%d%s",&aadharno,rname,,&age,bgroup);
+                   while(!feof(fp))
+                   {
+                         printf("\n%20lld%-20s%-20d%s ",aadharno,rname,age,bgroup);
+                         fscanf(fp,"%lld%s%d%s",aadharno,rname,&age,bgroup);
+                   }
+                   fclose(fp);
+      }
+     menu_admin();
 }
