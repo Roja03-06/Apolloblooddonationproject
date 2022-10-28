89905d6169b0ae0a375e6ffbf36b83b805b8ba5e
Roja03-06 <85451419+Roja03-06@users.noreply.github.com>
2022-10-28 19:02:28 +0530

    Update record.c

--- project/CUT/src/record.c
+++ project/CUT/src/record.c
@@ -16,23 +16,18 @@
 void records( )
 {
 	FILE *fr = fopen("../data/donor.txt","r");
-	
-	
 	long long  adno;
 	int units , ag;
 	char name[20],bg[20];
 	fscanf(fr,"%lld%s%d%s%d",&adno,name,&ag,bg,&units);
 	char b[10];
-
 	printf("\nEnter the blood group ");
 	scanf("%s",b);	
-	
 	int total=0;
 	while(!feof(fr))
 	{
 		if(strcmp(b,bg) == 0)
-		total=total+units;	
-					
+		total=total+units;			
 		fscanf(fr,"%lld%s%d%s%d",&adno,name,&ag,bg,&units);
 	}
 		printf("total no of %s =%d units",b,total);
